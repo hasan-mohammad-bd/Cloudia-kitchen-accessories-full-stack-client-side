39ace462347f1a2a65c87ce17a0c37da4401b592
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-22 15:40:40 +0600

    addng components

---
+++ src/components/Dashboard/ManageAllProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageAllProduct = () => {
+    return (
+        <div>
+            <h2>Manage all products</h2>
+        </div>
+    );
+};
+
+export default ManageAllProduct;
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDigrpkgX6F9H7tQjfRfdlJCmnetMSMjgk",
+  authDomain: "manufacturer-app-dff29.firebaseapp.com",
+  projectId: "manufacturer-app-dff29",
+  storageBucket: "manufacturer-app-dff29.appspot.com",
+  messagingSenderId: "378702944718",
+  appId: "1:378702944718:web:828a423bb74f5e0286339f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
