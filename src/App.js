06f8f92468340fcda75b3e40247a3c37288a210a
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-22 16:17:51 +0600

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Shop from './components/Shop';
+import Blogs from './components/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
+import MyPortfolio from './components/MyPortfolio';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/myportfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div>
-            <h2>This is navbar</h2>
+            <div className='flex justify-between container mx-auto'>
+                <div className="img-holder">
+                    <img src="" alt="" />
+                    <h2 className='text-2xl'>Hammer Grilled</h2>
+                </div>
+                <div className='nav-links'>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/shop'>Shop</NavLink>
+                    <NavLink to='/blogs'>Blogs</NavLink>
+                    <NavLink to='/dashboard'>Dashboard</NavLink>
+                    <NavLink to='/myportfolio'>My Portfolio</NavLink>
+                    <NavLink to='/login'>Login</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
